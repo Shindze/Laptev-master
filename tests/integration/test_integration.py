d8d0d69b7e9bf1def6e059df80429993a7f5a08a
Koviello Arlekkino <vainglory.ru@mail.ru>
2024-04-16 17:44:15 +0300

    Update test_integration.py

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,24 +1,34 @@
 import asyncpg
 import requests
+import pytest
+from pathlib import Path
+import sys
 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+sys.path.append(str(BASE_DIR))
+sys.path.append(str(BASE_DIR / 'favourite_service/app'))
+sys.path.append(str(BASE_DIR / 'movies_service/app'))
+
+from favourite_service.app.main import service_alive as favourite_service_status
+from movies_service.app.main import service_alive as movies_service_status
+
+@pytest.mark.asyncio
 async def test_database_connection():
     try:
         connection = await asyncpg.connect("postgresql://secUREusER:StrongEnoughPassword)@51.250.26.59:5432/query")
-        assert connection.is_connected()
+        assert connection
         await connection.close()
     except Exception as e:
         assert False, f"Не удалось подключиться к базе данных: {e}"
 
-async def test_kinopoisk_api():
-    headers = {"X-API-KEY": "700211e1-f970-499f-9957-6bca24e2adb1"}
-    r = requests.get("https://kinopoiskapiunofficial.tech/api/v2.2/films/500", headers=headers)
-    assert r.status_code == 200
 
-async def test_movies_service_connection():
-    r = requests.get("http://localhost:8000/health")
+@pytest.mark.asyncio
+async def test_favourite_service_connection():
+    r = await favourite_service_status()
     assert r == {'message': 'service alive'}
 
-async def test_favourite_service_connection():
-    r = requests.get("http://localhost:8001/health")
+@pytest.mark.asyncio
+async def test_movies_service_connection():
+    r = await movies_service_status()
     assert r == {'message': 'service alive'}
